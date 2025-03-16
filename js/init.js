ada614ecaf8db3e8b7d36f68fbe5f602370004ce
prufirebase2025 <prufirebase2025@gmail.com>
2025-03-15 18:25:24 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,16 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyCyaXSphRPAI3OwFMn6Ue2pcNPkWc74joo",
-  authDomain: "prufirebase20.firebaseapp.com",
-  projectId: "prufirebase20",
-  storageBucket: "prufirebase20.firebasestorage.app",
-  messagingSenderId: "813876877966",
-  appId: "1:813876877966:web:10b630e0cc33c9b2bb47df",
-  measurementId: "G-ZPB97413DE"
-    });
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js'
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js'
+// Add Firebase products that you want to use
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js'
+      // Initialize Firebase
+const firebaseConfig = {
+        apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
+  authDomain: "autenfb2023-5d562.firebaseapp.com",
+  projectId: "autenfb2023-5d562",
+  storageBucket: "autenfb2023-5d562.appspot.com",
+  messagingSenderId: "1089728085495",
+  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
+  measurementId: "G-S1T5SCE6JP"
+      };
